3034118662dfd33b65ccd29899adc3f611d90970
RSRiyad <salehin>
2024-05-24 02:53:09 +0600

    first commit

---
+++ socialmedia/userauth/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path
+from socialmedia import settings
+from userauth import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home),
+    path('loginn/',views.loginn),
+    path('signup/',views.signup,name='signup'),
+    path('logoutt/',views.logoutt),
+    path('upload',views.upload),
+    path('#<str:id>', views.home_post),
+    path('profile/<str:id_user>', views.profile),
+    path('delete/<str:id>', views.delete),
+    path('search-results/', views.search_results, name='search_results'),
+    path('follow', views.follow, name='follow'),    
+]
+
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
